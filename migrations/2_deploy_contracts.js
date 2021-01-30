65a9f5b575e4f2211e72d0b6f7375b1de47a36ef
flairxy <okonkwo.miracle@raadaa.com>
2021-01-30 13:00:23 +0100

    initial comit (backend + test only}

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,24 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
-const TutorialToken = artifacts.require("TutorialToken");
-const ComplexStorage = artifacts.require("ComplexStorage");
+const StateMachine = artifacts.require("StateMachine");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
-  deployer.deploy(ComplexStorage);
+module.exports = function (deployer, _network, accounts) {
+  deployer.deploy(
+    StateMachine,
+    web3.utils.toWei("1"), //amount
+    web3.utils.toWei("1", "Finney"), //interest. 1000 Finney = 1 Ether
+    30, //duration (s)
+    accounts[1], //borrower
+    accounts[2] //lender
+  );
 };
+
+// for test only
+// module.exports = function (deployer, _network, accounts) {
+//   deployer.deploy(
+//     StateMachine,
+//     1000,
+//     100,
+//     100,
+//     accounts[1], //borrower
+//     accounts[2] //lender
+//   );
+// };
--- vapp/src/drizzleOptions.js
+++ vapp/src/drizzleOptions.js
@@ -1,6 +1,4 @@
-import SimpleStorage from "./contracts/SimpleStorage.json";
-import ComplexStorage from "./contracts/ComplexStorage.json";
-import TutorialToken from "./contracts/TutorialToken.json";
+import StateMachine from "./contracts/StateMachine.json";
 
 const options = {
   web3: {
@@ -10,9 +8,9 @@ const options = {
       url: "ws://127.0.0.1:9545",
     },
   },
-  contracts: [SimpleStorage, ComplexStorage, TutorialToken],
+  contracts: [StateMachine],
   events: {
-    SimpleStorage: ["StorageSet"],
+    // SimpleStorage: ["StorageSet"],
   },
   polls: {
     accounts: 15000,
